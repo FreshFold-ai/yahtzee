fb0e12f44c54c58cd23e19213b3db31568633ab6
Urvish Patel <patelu@bgsu.edu>
2025-10-29 05:18:14 +0000

    Edit dice.cpp

---
+++ src/dice.cpp
@@ -0,0 +1,11 @@
+#include <random>
+
+class Dice {
+public:
+    int roll() {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dist(1, 6);
+        return dist(gen);
+    }
+};
