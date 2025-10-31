9bbadd1c89e275532e1e7788398f5f9a1159ee93
ElReyVanidoso <67380236+ElReyVanidoso@users.noreply.github.com>
2025-10-30 21:52:56 -0400

    Made headway on game.cpp/h and scorecard.cpp/h

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,33 @@
 #include "game.h"
 
-Game::Game() : totalScore(0) {}
+Game::Game()  
+{
+	std::fill(gameDice, gameDice+5, 0);
+	std::fill(faceValues, faceValues+5, 0);
+}
 
 int Game::getTotalScore() const {
-    return totalScore;
+	return this.totalScore;
 }
+int Game::getCurrScore() const{
+	return this.currRound;
+}
+
+void Game::play(){
+	this.currRound += 1;
+}
+
+void Game::roll5(){
+	for(Dice die: this.gameDice){
+		die.roll();
+	}
+}
+
+std::array<int, 5> Game::getFaceValues(){
+	int i = 0;
+	for(Dice die: this.gameDice){
+		this.faceValues[i] = die.getFaceValue();
+	}
+	return this.faceValues;
+}
+
