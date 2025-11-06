eddcbfcf7a65509f58861518b377d2e99549acc3
Urvish Patel <patelu@bgsu.edu>
2025-11-06 01:53:39 +0000

    Edit game.h

--- src/game.h
+++ src/game.h
@@ -10,20 +10,18 @@ public:
     Game();
     void play();
     void roll5();
-    std::array<int, 5> getFaceValues();
+    void setFaceValues();
     int getCurrRound();
     int getCurrScore();
-    int getTotalScore();
-    void pickSlot(int slot);
-    void assignPoints(int slot);
-    void sortDice();
+    int getTotalScore(int player);
+    void pickSlot(int player, int slot, std::array<int, 5> values);
 
 private:
     int totalScore(0);
     int currRound(0);
     int maxRound(13) const;
     std::array<int, 5> faceValues;
-    std::array<int, 5> sortedDice;
+    std::array<int, 5> sortedValues;
     std::arry<Dice, 5> gameDice;
     Scorecard scorecard;
 };
