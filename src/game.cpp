16331e39aa55a880d6c06451cb2b9a8f0ce2462a
Urvish Patel <patelu@bgsu.edu>
2025-11-06 01:53:31 +0000

    Edit game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -4,6 +4,7 @@ Game::Game()
 {
 	std::fill(gameDice, gameDice+5, 0);
 	std::fill(faceValues, faceValues+5, 0);
+	std::fill(sortedValues, sortedValues+5, 0);
 }
 
 int Game::getTotalScore() const {
@@ -17,17 +18,26 @@ void Game::play(){
 	this.currRound += 1;
 }
 
+//roll5 rolls each of the 5 die, 
+//and then this.setFaceValues() updates FaceValues to match the most recent roll
+//and simultaneously updates the sortedValues array. 
 void Game::roll5(){
 	for(Dice die: this.gameDice){
 		die.roll();
 	}
+	this.setFaceValues();
 }
 
-std::array<int, 5> Game::getFaceValues(){
+void Game::setFaceValues(){
 	int i = 0;
 	for(Dice die: this.gameDice){
 		this.faceValues[i] = die.getFaceValue();
 	}
-	return this.faceValues;
+	std::array<int, 5> face_copy = this.faceValues;
+	this.sortedValues = std::sort(face_copy.begin(), face_copy.end());
 }
+
+
+
+
 
